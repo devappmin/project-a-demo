fbd108085a4c1924a7d816eee1cfc1fabfaee2b8
Kim Seung Hwan <dev.appmin@gmail.com>
2026-08-15 21:24:46 +0900

    fix: complete player keyboard bindings

--- tests/unit/player/test_player_input.gd
+++ tests/unit/player/test_player_input.gd
@@ -3,6 +3,11 @@ extends "res://tests/support/test_case.gd"
 const GameMode = preload("res://app/session/game_mode.gd")
 const PlayerInput = preload("res://game/actors/player/player_input.gd")
 
+func _key_event(keycode: Key) -> InputEventKey:
+	var event := InputEventKey.new()
+	event.keycode = keycode
+	return event
+
 func run() -> void:
 	GameSession.change_mode(GameMode.Value.EXPLORATION)
 	Input.action_press(&"move_right")
@@ -19,3 +24,13 @@ func run() -> void:
 	Input.action_release(&"sprint")
 	Input.action_release(&"interact")
 	GameSession.change_mode(GameMode.Value.EXPLORATION)
+	assert_true(InputMap.event_is_action(_key_event(KEY_A), &"move_left", true), "A moves left")
+	assert_true(InputMap.event_is_action(_key_event(KEY_LEFT), &"move_left", true), "left arrow moves left")
+	assert_true(InputMap.event_is_action(_key_event(KEY_D), &"move_right", true), "D moves right")
+	assert_true(InputMap.event_is_action(_key_event(KEY_RIGHT), &"move_right", true), "right arrow moves right")
+	assert_true(InputMap.event_is_action(_key_event(KEY_W), &"move_up", true), "W moves up")
+	assert_true(InputMap.event_is_action(_key_event(KEY_UP), &"move_up", true), "up arrow moves up")
+	assert_true(InputMap.event_is_action(_key_event(KEY_S), &"move_down", true), "S moves down")
+	assert_true(InputMap.event_is_action(_key_event(KEY_DOWN), &"move_down", true), "down arrow moves down")
+	assert_true(InputMap.event_is_action(_key_event(KEY_SHIFT), &"sprint", true), "Shift sprints")
+	assert_true(InputMap.event_is_action(_key_event(KEY_E), &"interact", true), "E interacts")
